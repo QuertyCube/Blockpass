d86839df45953e7e4cf3e33b8a573ca224482f70
Kevin <kevinqwerty23@gmail.com>
2025-02-20 02:10:49 +0700

    finalize first initial test

--- test/Master.t.sol
+++ test/Master.t.sol
@@ -1,23 +1,48 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.19;
 
 import "forge-std/Test.sol";
 import "../src/Master.sol";
+import "../src/EventContract.sol";
 
-contract MasterTest is Test {
-    MasterContract master;
-    address owner;
-    address addr1;
-    address addr2;
+contract MasterContractTest is Test {
+    MasterContract masterContract;
+    address treasuryContract = address(0x123);
+    address usdcToken = address(0x456);
+    address vendor = address(0x789);
 
     function setUp() public {
-        // master = new MasterContract(input tresery contrac);
-        owner = address(this);
-        addr1 = address(0x1);
-        addr2 = address(0x2);
+        masterContract = new MasterContract(treasuryContract);
+        masterContract.addVendor(vendor);
     }
 
-    function testCreate() public {
-        // assertEq(master.owner(), owner);
+    function testCreateEvent() public {
+        address newEventAddress = createEvent();
+
+        // Verify the event was created
+        assertTrue(newEventAddress != address(0));
+        assertEq(masterContract.eventContracts(0), newEventAddress);
+    }
+    function createEvent() internal returns (address) {
+        // Prepare the EventParams
+        MasterContract.TicketInfo[] memory ticketInfos = new MasterContract.TicketInfo[](2);
+        ticketInfos[0] = MasterContract.TicketInfo({ticketType: "VIP", price: 100 * 10**6, maxSupply: 100});
+        ticketInfos[1] = MasterContract.TicketInfo({ticketType: "Regular", price: 50 * 10**6, maxSupply: 500});
+
+        MasterContract.EventParams memory params = MasterContract.EventParams({
+            name: "My Event",
+            nftSymbol: "MEVT",
+            start: 1672531200,
+            end: 1672617600,
+            startSale: 1672444800,
+            endSale: 1672527600,
+            ticketInfos: ticketInfos,
+            usdcToken: usdcToken
+        });
+
+        // Call createEvent as the vendor
+        vm.prank(vendor);
+        address newEventAddress = masterContract.createEvent(params);
+        return newEventAddress;
     }
 }
