36a8f72740f9e31e3b0c53c1fcc45fc7f657568a
Kevin <kevinqwerty23@gmail.com>
2025-03-22 14:02:33 +0700

    Update deploy.s.sol

--- script/deploy.s.sol
+++ script/deploy.s.sol
@@ -11,20 +11,23 @@ import {MockERC20} from "../src/MockERC20.sol";
 contract DeployScript is Script {
     function run() external {
 
-        address usdc_token_testnet_address = address(0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238); // USDC testnet address
-        address usdc_token_mainnet_address = address(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48); // USDC mainnet address
+        address eth_usdc_token_testnet_address = address(0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238); // USDC testnet address
+        address eth_usdc_token_mainnet_address = address(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48); // USDC mainnet address
+
+        address base_usdc_token_testnet_address = address(0x036CbD53842c5426634e7929541eC2318f3dCF7e); // USDC testnet address
 
         // Ambil private key dan RPC URL dari .env
         uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
-        string memory network = vm.envString("NETWORK");
+        string memory chain = "BASE"; // BASE, BSC, POLYGON, etc
+        string memory network = "SEPOLIA"; // SEPOLIA, MAINNET
         string memory rpcUrl;
 
-        if (keccak256(bytes(network)) == keccak256(bytes("sepolia"))) {
-            rpcUrl = vm.envString("SEPOLIA_RPC_URL");
-            console.log(" Deploying to Sepolia...");
+        if (keccak256(bytes(network)) == keccak256(bytes("SEPOLIA"))) {
+            rpcUrl = vm.envString(string.concat(chain, "_SEPOLIA_RPC_URL"));
+            console.log(" Deploying to ",chain," Sepolia...");
         } else if (keccak256(bytes(network)) == keccak256(bytes("mainnet"))) {
-            rpcUrl = vm.envString("MAINNET_RPC_URL");
-            console.log(" Deploying to Ethereum Mainnet...");
+            rpcUrl = vm.envString(string.concat(chain, "_MAINNET_RPC_URL"));
+            console.log(" Deploying to ",chain," Mainnet...");
         } else {
             revert(" Unsupported network! Use 'sepolia' or 'mainnet'.");
         }
@@ -42,7 +45,7 @@ contract DeployScript is Script {
         // Deploy MasterContract
         MasterContract masterContract = new MasterContract(
             address(treasuryFund),
-            usdc_token_testnet_address,
+            base_usdc_token_testnet_address,
             address(masterOwnerModifier)
         );
         console.log("MasterContract deployed at:", address(masterContract));
